8c0f01ea6d3a7e523acb12786aa5feeb3bc060a6
Leonardo Ramirez <deiividramirez@gmail.com>
2023-02-11 10:47:02 -0600

    Modif - Python aux_plot.py file

--- src/data/aux_plot.py
+++ src/data/aux_plot.py
@@ -15,7 +15,16 @@ import os
 from pathlib import Path
 path = Path(__file__).parent.absolute()
 
-dron = sys.argv[1] if len(sys.argv) > 1 else "1"
+lider = 0
+if len(sys.argv) == 2:
+    dron = sys.argv[1]
+elif len(sys.argv) == 3:
+    lider = sys.argv[1]
+    dron = sys.argv[2]
+else:
+    print("Usage: python3 aux_plot.py [dron]")
+    print("Usage: python3 aux_plot.py [if lider 1 else 0)] [dron]")
+    exit()
 
 err = np.loadtxt(f"{path}/out/out_errors_{dron}.txt")
 err_pix = np.loadtxt(f"{path}/out/out_errors_pix_{dron}.txt")
@@ -39,67 +48,100 @@ for i in range(2, len(vx)):
 
 
 print(f"""
-
-
 INFORMACIÓN DEL EXPERIMENTO
 
-Tiempo de vuelo: {time[-1]} s
-Error promedio: {np.mean(err[NUM:])} pixeles
-Velocidad media en x: {np.mean(vx[NUM:])} u/s
-Velocidad media en y: {np.mean(vy[NUM:])} u/s
-Velocidad media en z: {np.mean(vz[NUM:])} u/s
-Velocidad media en yaw: {np.mean(vyaw[NUM:])} u/s
-Lambda promedio: {np.mean(lamb[NUM:])}
+Tiempo de vuelo: {time[-1]:5f} s
+Error promedio: {np.mean(err[NUM:]):5f} pixeles
+Velocidad media en x: {np.mean(vx[NUM:]):5f} u/s
+Velocidad media en y: {np.mean(vy[NUM:]):5f} u/s
+Velocidad media en z: {np.mean(vz[NUM:]):5f} u/s
+Velocidad media en yaw: {np.mean(vyaw[NUM:]):5f} u/s
+Lambda promedio: {np.mean(lamb[NUM:]):5f}
 
-Error final: {err[-1]} pixeles
-Velocidad final en x: {vx[-1]} u/s
-Velocidad final en y: {vy[-1]} u/s
-Velocidad final en z: {vz[-1]} u/s
-Velocidad final en yaw: {vyaw[-1]} u/s""")
+Error final: {err[-1]:5f} pixeles
+Velocidad final en x: {vx[-1]:5f} u/s
+Velocidad final en y: {vy[-1]:5f} u/s
+Velocidad final en z: {vz[-1]:5f} u/s
+Velocidad final en yaw: {vyaw[-1]:5f} u/s""" if lider == "1" else f"""
+INFORMACIÓN DEL EXPERIMENTO
 
-fig, ax = plt.subplots(3, 1, figsize=(6, 10))
-# fig.canvas.set_window_title('Resultados del experimento')
+Tiempo de vuelo: {time[-1]:5f} s
+Velocidad media en x: {np.mean(vx[NUM:]):5f} u/s
+Velocidad media en y: {np.mean(vy[NUM:]):5f} u/s
+Velocidad media en z: {np.mean(vz[NUM:]):5f} u/s
+Velocidad media en yaw: {np.mean(vyaw[NUM:]):5f} u/s
+Lambda promedio: {np.mean(lamb[NUM:]):5f}
 
-ax[0].plot(time[NUM:], err[NUM:], "purple", label='Error (c)')
-err_pix = err_pix / max(err_pix)
-ax[0].plot(time[NUM:], err_pix[NUM:], "r", label='Error (px)')
-box = ax[0].get_position()
-ax[0].set_position([box.x0, box.y0, box.width * 0.99, box.height])
-ax[0].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-# ax[0].legend(loc='best', shadow=True)
-ax[0].set_ylabel('Error Promedio')
+Velocidad final en x: {vx[-1]:5f} u/s
+Velocidad final en y: {vy[-1]:5f} u/s
+Velocidad final en z: {vz[-1]:5f} u/s
+Velocidad final en yaw: {vyaw[-1]:5f} u/s""")
 
+if lider == "1":
+    # PRIMER PLOT
+    fig, ax = plt.subplots(3, 1, figsize=(5, 8))
 
-ax[1].plot(time[NUM:], vx[NUM:], label='$V_x$')
-ax[1].plot(time[NUM:], vy[NUM:], label='$V_y$')
-ax[1].plot(time[NUM:], vz[NUM:], label='$V_z$')
-ax[1].plot(time[NUM:], vyaw[NUM:], label='$W_z$')
-ax[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-ax[1].set_ylabel('Velocidades')
+    ax[0].plot(time[NUM:], err[NUM:], "purple", label='Error (c)')
+    err_pix = err_pix / max(err_pix)
+    ax[0].plot(time[NUM:], err_pix[NUM:], "r", label='Error (px)')
+    box = ax[0].get_position()
+    ax[0].set_position([box.x0, box.y0, box.width * 0.99, box.height])
+    ax[0].legend(loc='center left', bbox_to_anchor=(1, 0.5), shadow=True)
+    ax[0].set_ylabel('Error Promedio')
 
+    ax[1].plot(time[NUM:], vx[NUM:], label='$V_x$')
+    ax[1].plot(time[NUM:], vy[NUM:], label='$V_y$')
+    ax[1].plot(time[NUM:], vz[NUM:], label='$V_z$')
+    ax[1].plot(time[NUM:], vyaw[NUM:], label='$W_z$')
+    ax[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax[1].set_ylabel('Velocidades')
 
-ax[2].plot(time[NUM:], lamb[NUM:], "r")
-ax[2].set_ylabel('Lambda')
-ax[2].set_xlabel('Tiempo (s)')
+    ax[2].plot(time[NUM:], lamb[NUM:], "r")
+    ax[2].set_ylabel('Lambda')
+    ax[2].set_xlabel('Tiempo (s)')
 
-plt.tight_layout()
-plt.savefig(f"{path}/out_velocidades.png", bbox_inches='tight', pad_inches=0.1)
+    plt.tight_layout()
+    plt.savefig(f"{path}/out_velocidades_{dron}.png",
+                bbox_inches='tight', pad_inches=0.1)
 
+    # SEGUNDO PLOT
+    fig, ax = plt.subplots(2, 1, figsize=(5, 6))
+    oslist = os.listdir(f"{path}/img")
+    filtered = [i for i in oslist if f"{dron}_" in i]
+    imgs = sorted(filtered, key=lambda x: int(x.split(".")[0]))
 
-fig, ax = plt.subplots(2, 1, figsize=(5, 10))
-imgs = sorted(os.listdir(f"{path}/img"), key=lambda x: int(x.split(".")[0]))
-des = f"{path.parent.absolute()}/desired_{dron}f.jpg"
+    des = f"{path.parent.absolute()}/desired_{dron}f.jpg"
 
-ax[0].imshow(plt.imread(des))
-ax[0].set_ylabel("Imagen deseada")
-if len(imgs) > 0:
-    ax[1].imshow(plt.imread(f"{path}/img/{imgs[-1]}"))
-    ax[1].set_ylabel("Imagen final")
+    ax[0].imshow(plt.imread(des))
+    ax[0].set_ylabel("Imagen deseada")
+    if len(imgs) > 0:
+        ax[1].imshow(plt.imread(f"{path}/img/{imgs[-1]}"))
+        ax[1].set_ylabel("Imagen final")
 
-[i.set_xticks([]) for i in ax]
-[i.set_yticks([]) for i in ax]
+    [i.set_xticks([]) for i in ax]
+    [i.set_yticks([]) for i in ax]
 
-plt.tight_layout()
-plt.savefig(f"{path}/out_imagenes.png", bbox_inches='tight', pad_inches=0.1)
+    plt.tight_layout()
+    plt.savefig(f"{path}/out_imagenes_{dron}.png",
+                bbox_inches='tight', pad_inches=0.1)
+
+else:
+    fig, ax = plt.subplots(2, 1, figsize=(5, 5))
+
+    ax[0].plot(time[NUM:], vx[NUM:], label='$V_x$')
+    ax[0].plot(time[NUM:], vy[NUM:], label='$V_y$')
+    ax[0].plot(time[NUM:], vz[NUM:], label='$V_z$')
+    ax[0].plot(time[NUM:], vyaw[NUM:], label='$W_z$')
+    ax[0].legend(loc='center left', bbox_to_anchor=(1, 0.5), shadow=True)
+    ax[0].set_ylabel('Velocidades')
+
+    ax[1].plot(time[NUM:], lamb[NUM:], "r")
+    ax[1].set_ylabel('Lambda')
+    ax[1].set_xlabel('Tiempo (s)')
+
+    plt.tight_layout()
+    plt.savefig(f"{path}/out_velocidades_{dron}.png",
+                bbox_inches='tight', pad_inches=0.1)
+
 
 plt.show()
